1cc8d2bfb89c8f84dd9baf3d2a88e417498f551e
rakshakarbhari16 <rakshakarbhari16@gmail.com>
2024-08-01 09:40:02 +0530

    created Footer Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Header />
+     <>
+     <Header />
+     <Container>
+      <main></main>
+     </Container>
+     <Footer />
+     </>
+
+    
   );
 }
 
